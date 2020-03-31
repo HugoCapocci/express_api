a019ac52e7b5f8d611eec7962d4f58e030b44c32
Hugo CAPOCCI <h.capocci@gmail.com>
2020-03-31 11:59:26 +0200

    Create POST route

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 const app = express();
 const v1 = express.Router();
 
+// toujours garder bodyParser en premier dans les appels à use()
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/api/v1', v1);
 
 // request : requette HTTP (reçu du client)
@@ -24,6 +28,28 @@ v1.get('/message/:id', async (request, response) => {
 
     // ternaire
     quote ? response.send(quote) : response.sendStatus(404);
+});
+
+v1.post('/message', async (request, response) => {
+    const message = request.body;
+
+    // un message  est valide si il a un auteur et une citation
+    const isValid = message.quote && message.quote.length > 0
+     && message.author && message.author.length > 0;
+    
+    if (!isValid) return response.sendStatus(400);
+
+    const quotes = await fs.readFile('./data/quotes.json');
+    if (!quotes) return response.sendStatus(500);
+
+    const quoteArray = JSON.parse(quotes);
+    quoteArray.sort((quoteA, quoteB) => quoteB.id - quoteA.id);
+    /* équivalent de: function(quoteA, quoteB) {
+        return quoteB - quoteA;
+    }
+    */
+    message.id = quoteArray[0].id + 1;
+    response.send(message);
 });
 
 app.listen(3000, () => {
