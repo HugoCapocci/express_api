fa7c6bd49409e953ce87922d9f77b17c9ea81978
Hugo CAPOCCI <h.capocci@gmail.com>
2020-03-31 11:11:44 +0200

    create second REST route, to request ONE quote

--- index.js
+++ index.js
@@ -8,9 +8,22 @@ app.use('/api/v1', v1);
 // request : requette HTTP (reçu du client)
 // response : response HTTP (à envoyer au client, en retour)
 v1.get('/message', async (request, response) => {
-
     const quotes = await fs.readFile('./data/quotes.json');
-    response.send(quotes);
+    response.send(JSON.parse(quotes));
+});
+
+v1.get('/message/:id', async (request, response) => {
+    const quotes = await fs.readFile('./data/quotes.json');
+    const quoteArray = JSON.parse(quotes);
+
+    // recupérer la citation qui correspond à l'id transmis
+    const id = request.params.id;
+    const quote = quoteArray.find(function(currentQuote) {
+       return currentQuote.id == id;
+    });
+
+    // ternaire
+    quote ? response.send(quote) : response.sendStatus(404);
 });
 
 app.listen(3000, () => {
