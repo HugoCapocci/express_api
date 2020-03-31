f390238e8d7ee5a3fdd2e1e7c7cc8604c9ef9d82
AbdelrhmanAbdalla <abdelrhman92.abdalla@gmail.com>
2020-03-31 11:14:14 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const fs = require('fs').promises;
+
+const app = express();
+const v1 = express.Router();
+
+app.use('/api/v1', v1);
+
+// request : requette HTTP (reçu du client)
+// response : response HTTP (à envoyer au client en retour)
+v1.get('/message', async (request, response) => {
+    const quotes = await fs.readFile('./data/quotes.json');
+    response.send(JSON.parse(quotes));
+});
+
+v1.get('/message/:id', async (request, response) => {
+    const quotes = await fs.readFile('./data/quotes.json');
+    const quotesArray = JSON.parse(quotes);
+
+    //recuperer la citation qui correspond a l'id transmis
+    const id = request.params.id;
+    const quote = quoteArray.find(function(currentQuote){
+        return currentQuote.id == id;
+    });
+
+    if(!quote){
+        response.sendStatus(404);
+    }else{
+        response.send(quote);
+    }
+
+});
+
+app.listen(3000, () =>{
+    console.log('Server listening on port 3000')
+});
