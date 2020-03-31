f2c2248ed6fd0755702816be2eeca73e1537c5a3
Tony PEREIRA <61658422+pereira-tony@users.noreply.github.com>
2020-03-31 12:28:42 +0200

    Création de la route "POST" avec un fichier JSON

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 const app = express();
 const v1 = express.Router();
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.use('/api/v1', v1);
 
 // request : requête HTTP (reçu du client)
@@ -23,6 +26,34 @@ v1.get('/message/:id', async (request, response) => {
     })
     // Opération ternaire
     quote ? response.send(quote) : response.sendStatus(404);
+})
+
+v1.post('/message', async (request, response) => {
+    const message = request.body;
+    // Un message est valide si il a un auteur et une citation
+    console.log('message ?', message);
+
+    // Contrôle
+    const isValid = message.quote && message.quote.length > 0 && message.author && message.author.length > 0;
+
+    // Sil n'y a pas de quotes
+    if (!quotes) return response.sendStatus(500);
+
+    //Si pas valide
+    if (!isValid) return response.sendStatus(400);
+    const quotes = await fs.readFile('./data/quotes.json');
+    const quoteArray = JSON.parse(quotes);
+
+    /* 
+    équivalent de fonction (quoteA, quoteB) {
+        return quoteB - quoteA;
+    }
+    */
+    quoteArray.sort((quoteA, quoteB) => quoteB.id - quoteA.id);
+   
+    //Si valide : renvoit l'élément créé
+    message.id = quoteArray[0].id + 1;
+    response.send(message);
 })
 
 app.listen(3000, () => {
