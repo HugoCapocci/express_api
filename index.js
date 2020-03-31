a6156d24d0795b28479e3c1ac96b1561d64bba52
Winda <azis.widad@gmail.com>
2020-03-31 12:28:11 +0200

    GET - POST API

--- index.js
+++ index.js
@@ -1,25 +1,63 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 const app = express();
 const v1 = express.Router();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/api/v1', v1);
 
 // req = requete HTTP (reçu du client)
 // res = response HTTP (à envoyer au client en retour)
-v1.get('/message', (req, res) => {
-    res.send([
-            {
-                "quote": "test oui un deux trois",
-                "author": "Winda",
-                "id": 1
-            },
-            {
-                "quote": "les cours en ligne ... pas ouf ca bug",
-                "author": "Hitema",
-                "id": 2
-            }
-    ])
+v1.get('/message', async (req, res) => {
+        const quotes = await fs.readFile('./data/quotes.json');
+        //res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.parse(quotes));
+});
+
+v1.get('/message/:id', async (req, res) => {
+    const quotes = await fs.readFile('./data/quotes.json');
+    const quoteArray = JSON.parse(quotes);
+    // récupérer le message qui correspond à l'id
+    const id = req.params.id;
+    const quote = quoteArray.find(function(currentQuote) {
+        return currentQuote.id == id;
+    });
+
+    if (!quote) {
+        res.sendStatus(404);
+    } else {
+        res.send(quote);
+    }
+
+});
+
+v1.post('/message', async (req, res) => {
+    const message = req.body;
+    // un message est valide s'il a un auteur et une citation
+    console.log('message?', message);
+
+    const isValid = message.quote && message.quote.length > 0
+        && message.author && message.author.length > 0;
+    
+    // si pas valide :
+    if (!isValid)
+        return res.sendStatus(400);
+
+    const quotes = await fs.readFile('./data/quotes.json');
+    const quoteArray = JSON.parse(quotes);
+    quoteArray.sort((quoteA, quoteB) => quoteB.id - quoteA.id);
+    /*
+        Equivalent de : function(a,b) {
+            return b - a;
+        } 
+    */
+
+    // si valide : renvoit l'élément créé
+    // ajouter max id + 1
+    message.id = quoteArray[0].id + 1;
+    res.send(message);
 });
 
 app.listen(3000, () => {
