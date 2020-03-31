cc623dad495f7362e5ae597367f85176af76b9ea
Audrey <audrey-lopes-correia@hotmail.fr>
2020-03-31 12:24:45 +0200

    methode post ok

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
-const fs = require('fs').promises;
 const v1 = express.Router();
 
-app.use('/api/v1', v1); 
+//toujours garder bodyParser en premier dans les appels a use()
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/api/v1', v1);
+
 
 //request = requete http ( reçu du client )
 //response = reponse HTTP ( à envoyer au client en retour )
@@ -12,7 +17,7 @@ v1.get('/message', async (request, response)=>{
     const quotes = await fs.readFile('./data/quotes.json');
     response.send(JSON.parse(quotes));
 
-})
+});
 
 v1.get('/message/:id', async (request, response)=>{
     const quotes = await fs.readFile('./data/quotes.json');
@@ -24,7 +29,30 @@ v1.get('/message/:id', async (request, response)=>{
     });
     quote ? response.send(quote) : response.sendStatus(404) ;
 
-})
+});
+
+v1.post('/message', async (request, response)=>{
+    const message =  request.body;
+    //une citation est valide si elle a un auteur et une citation
+    console.log('message', message);
+    const isValid = message.quote && message.author && message.quote.length > 0 && message.author.length > 0;
+    //si pas valide
+    if (!isValid) return response.sendStatus(400);
+
+    const quotes = await fs.readFile('./data/quotes.json');
+    const quoteArray = JSON.parse(quotes);
+    quoteArray.sort((quoteA,quoteB)=>quoteB.id-quoteA.id);
+    /*équivalent de : function(quoteA,quoteB){
+        return quoteB-quoteA;
+    }
+    */
+
+    //si valide: renvoit l'element crée
+    //ajouter max id+1
+    message.id=quoteArray[0].id+1 ;
+    response.send(message);
+
+});
 
 app.listen(3000,()=>{
     console.log('Server listening in port 3000!');
