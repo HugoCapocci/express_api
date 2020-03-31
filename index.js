f2e89fbcceee5ebc0cd048df2875894110b00bb8
Tony PEREIRA <61658422+pereira-tony@users.noreply.github.com>
2020-03-31 10:18:17 +0200

    optimisation de la route "GET" avec le readFile

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const fs = require('fs').promises;
 const app = express();
 const v1 = express.Router();
 
@@ -7,22 +7,11 @@ app.use('/api/v1', v1);
 
 // request : requête HTTP (reçu du client)
 // response : réponse HTTP (à envoyer au client, en retour)
-v1.get('/message', (request, response) => {
-    response.send(
-        [
-            {
-            "quote": "All mankind is of one author, and is one volume; when one man dies, one chapter is not torn out of the book, but translated into a better language; and every chapter must be so translated; God emploies several translators; some pieces are translated by age, some by sickness, some by war, some by justice; but God's hand is in every translation; and his hand shall bind up all our scattered leaves again, for that library where every book shall lie open to one another.",
-            "author": "John Donne",
-            "id": 1
-            },
-            {
-            "quote": "Goodness is always an asset. A man who is straight, friendly and useful may never be famous, but he is respected and liked by all who know him. He has laid a sound foundation for success and he will have a worthwhile life.",
-            "author": "Herbert N. Casson",
-            "id": 2
-            }
-            
-        ]
-    )
+v1.get('/message', async (request, response) => {
+
+    const quotes = await fs.readFile('./data/quotes.json');
+    response.send(quotes);
+
 });
 
 app.listen(3000, () => {
