99448b903c9c4b14e710b12185c38c3632c49f35
Issam Kadar <issam.kadar@gmail.com>
2020-03-31 13:48:23 +0200

    add route POST /message

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const fs = require('fs').promises;
 const app = express();
+const bodyParser = require('body-parser');
 
 const v1 = express.Router();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use('/api/v1', v1);
 
 v1.get('/message', async (request, response) => {
@@ -27,6 +30,28 @@ v1.get('/message/:id', async (request, response) => {
     quote ?  response.send(quote) : response.sendStatus(404);
 })
 
+v1.post('/message', async (request, response) => {
+    const message = request.body;    
+    const isValid = message.quote && message.quote.length > 0 && message.author
+
+    if (!isValid) return response.sendStatus(400);
+    const quotes = await fs.readFile('./data/quotes.json');
+    const quoteArray = JSON.parse(quotes);
+    quoteArray.sort((quoteA,quoteB) => quoteB.id - quoteA.id);
+
+    message.id = quoteArray[0].id + 1;
+    response.send(message);
+
+
+    console.log(message);
+})
+
 app.listen(3000, () => {
     console.log('Server is listening on port 3000')
 })
+
+function getNextId(obj) {
+    return (Math.max.apply(Math, obj.map(function(o) {
+        return o.id;
+    })) + 1);
+}
