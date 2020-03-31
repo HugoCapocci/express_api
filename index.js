72384ca97941bb8ff9e158ef60f4b352d79500e7
Issam Kadar <issam.kadar@gmail.com>
2020-03-31 11:11:37 +0200

    create GET route /message/:id

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
-const fs = require('fs');
+const fs = require('fs').promises;
 const app = express();
 
 const v1 = express.Router();
 
 app.use('/api/v1', v1);
 
-v1.get('/message', (request, response) => {
-    const quotes = await fs.readFile('./data/quotes.json')
+v1.get('/message', async (request, response) => {
+    try {
+        const quotes = await fs.readFile('./data/quotes.json');
+        response.send(JSON.parse(quotes));
+    } catch (error) {
+        response.send(error);
+    }
+})
 
-    response.send(quotes);
+v1.get('/message/:id', async (request, response) => {
+    const quotes = await fs.readFile('./data/quotes.json')
+    const quoteArray = JSON.parse(quotes);
+    const id = request.params.id;
+
+    const quote = await quoteArray.find((quote) => {
+        return quote.id == id
+    });
+
+    quote ?  response.send(quote) : response.sendStatus(404);
 })
 
 app.listen(3000, () => {
