cb6085f223d0665244ce419f19580357ccbac068
Elias HAMADOUCHE <elinole94@gmail.com>
2020-03-31 10:30:54 +0200

    Correction affichage quotes

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ app.use('/api/v1', v1);
 // request : requête HTTP (reçu du client)
 // response : response HTTP (à envoyer au client, en retour)
 v1.get('/message', async (request, response) => {
-    const quotes = await fs.readFile('./data/quotes.json')
-    response.send(quotes);
+    const quotes = await fs.readFile('./data/quotes.json');
+    response.send(JSON.parse(quotes));
 });
+
+v1.get('/message/:id', async (request, response) => {
+    const quotes = await fs.readFile('./data/quotes.json');
+    // récupérer la citation qui correspond à l'id transmis
+    const id = request.params.id;
+})
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000!');
