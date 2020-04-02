e0f646e9daa931bc27bdf91bc469dd5a3a6e100b
Stéphane <stephane.achari@primeradiant.fr>
2020-04-02 16:41:58 +0200

    Add post & delete message

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const v1 = express.Router();
 const port = process.env.PORT || 3000;
 const basicAuth = require('./middleware/basic-auth').basicAuth;
-const MessageService = require('./services/message-service').MessageService;
+const MessageService = require('./services/message-service');
 const messageService = new MessageService();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,17 +15,25 @@ app.use(bodyParser.json());
 app.use('/api/v1', v1);
 
 v1.get('/message', async (request, response) => {
-  const quotes = await fs.readFile('./data/quotes.json');
-  response.send(JSON.parse(quotes));
+  // const quotes = await fs.readFile('./data/quotes.json');
+  // response.send(JSON.parse(quotes));
+  const quotes = await messageService.getMessages();
+  response.send(quotes);
 });
 
 v1.get('/message/:id', async (request, response) => {
-  let quotes = await fs.readFile('./data/quotes.json');
-  quotes = JSON.parse(quotes);
-  const message = quotes.find(
-    quote => quote.id === parseInt(request.params.id),
-  );
-  !message ? response.sendStatus(404) : response.send(message);
+  // let quotes = await fs.readFile('./data/quotes.json');
+  // quotes = JSON.parse(quotes);
+  // const message = quotes.find(
+  //   quote => quote.id === parseInt(request.params.id),
+  // );
+  // !message ? response.sendStatus(404) : response.send(message);
+  try {
+    const message = await messageService.getMessage(request.params.id);
+    !message ? response.sendStatus(404) : response.send(message);
+  } catch (err) {
+    response.sendStatus(400);
+  }
 });
 
 v1.post('/message', basicAuth, async (request, response) => {
@@ -40,12 +48,25 @@ v1.post('/message', basicAuth, async (request, response) => {
     return response.sendStatus(500);
   }
 
-  const createdMessage = messageService.createMessage(message);
-  let quotes = await fs.readFile('./data/quotes.json');
-  quotes = JSON.parse(quotes);
-  const ids = quotes.map(quote => quote.id);
-  message.id = Math.max.apply(null, ids) + 1;
-  response.send(message);
+  const createdMessage = await messageService.createMessage(message);
+  response.send(createdMessage);
+
+  // let quotes = await fs.readFile('./data/quotes.json');
+  // quotes = JSON.parse(quotes);
+  // const ids = quotes.map(quote => quote.id);
+  // message.id = Math.max.apply(null, ids) + 1;
+  // response.send(message);
+});
+
+v1.delete('/message/:id', basicAuth, async (request, response) => {
+  try {
+    const deletedMessage = await messageService.deleteMessage(
+      request.params.id,
+    );
+    response.sendStatus(deletedMessage ? 200 : 204);
+  } catch (err) {
+    response.sendStatus(400);
+  }
 });
 
 app.listen(port, () => console.log(`App listning on port ${port}`));
--- services/message-service.js
+++ services/message-service.js
@@ -10,8 +10,39 @@ module.exports = class MessageService {
     return client.connect();
   }
 
-  createMessenger(message) {
-    const client = this.getConnectedClient();
-    client.close();
+  async createMessage(message) {
+    const client = await this.getConnectedClient();
+    const collection = client.db(process.env.MONGO_DB).collection('messages');
+    const insertedMessage = await collection.insertOne(message);
+    await client.close();
+
+    return insertedMessage;
+  }
+
+  async getMessages() {
+    const client = await this.getConnectedClient();
+    const collection = client.db(process.env.MONGO_DB).collection('messages');
+    const messages = await collection.find({}).toArray();
+    await client.close();
+
+    return messages;
+  }
+
+  async getMessage(id) {
+    const client = await this.getConnectedClient();
+    const collection = client.db(process.env.MONGO_DB).collection('messages');
+    const message = await collection.findOne({ _id: ObjectID(id) });
+    await client.close();
+
+    return message;
+  }
+
+  async deleteMessage(id) {
+    const client = await this.getConnectedClient();
+    const collection = client.db(process.env.MONGO_DB).collection('messages');
+    const deletedMessage = await collection.deleteOne({ _id: ObjectID(id) });
+    await client.close();
+
+    return deletedMessage.deletedCount === 1;
   }
 };
