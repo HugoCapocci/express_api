8a93041b5672771ed0b71ec35f205e7366d69da0
JulienQueant <julien.queant@u-psud.fr>
2020-04-02 16:27:29 +0200

    fix(messages): rewrite /get & /post

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express';
-import { promises as fs } from 'fs';
 
 import basicAuth from './middleware/basic-auth.js';
+
+import MessageService from './services/message.js';
+const messageService = new MessageService();
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const v1 = express.Router();
@@ -12,29 +17,27 @@ app.use(express.json());
 app.use('/api/v1', v1);
 
 v1.get('/message', async (req, res) => {
-    const rawQuotes = await fs.readFile('./data/quotes.json');
-    const quotes = JSON.parse(rawQuotes);
+    const quotes = await messageService.getMessages();
     res.send(quotes);
 });
 
 v1.get('/message/:id', async (req, res) => {
-    const rawQuotes = await fs.readFile('./data/quotes.json');
-    const quotes = JSON.parse(rawQuotes);
     const id = req.params.id;
-    const quote = quotes.find(quote => quote.id == id);
-    !quote ? res.sendStatus(404) : res.send(quote);
+    try {
+        const quote = await messageService.getMessage(id);
+        res.send(quote);
+    } catch (error) {
+        res.sendStatus(400);
+    }
 });
 
 v1.post('/message', basicAuth, async (req, res) => {
     const message = req.body;
-    const isValid = message.quote && message.author
+    const isValid = message.quote && message.author;
     if (!isValid) return res.sendStatus(400);
-    const rawQuotes = await fs.readFile('./data/quotes.json');
-    const quotes = JSON.parse(rawQuotes);
-    quotes.sort((q1, q2) => q2.id - q1.id);
-    message.id = quotes[0].id + 1;
-    res.send(message);
-})
+    const createdMessage = await messageService.createMessage(message);
+    res.send(createdMessage);
+});
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000!');
---
+++ services/message.js
@@ -0,0 +1,48 @@
+import mongodb from "mongodb";
+const { MongoClient } = mongodb
+
+class MessageService {
+    getConnectedService() {
+        const client = new MongoClient(
+            process.env.DATABASE_URL,
+            { useNewUrlParser: true, useUnifiedTopology: true }
+        );
+
+        return client.connect();
+    };
+
+    async createMessage(message) {
+        const client = await this.getConnectedService();
+
+        const collection = client.db(process.env.DATABASE_NAME).collection('messages')
+        const resultMessage = await collection.insertOne(message);
+
+        await client.close();
+
+        return resultMessage;
+    };
+
+    async getMessages() {
+        const client = await this.getConnectedService();
+
+        const collection = client.db(process.env.DATABASE_NAME).collection('messages');
+        const quotes = await collection.find({}).toArray();
+
+        await client.close();
+
+        return quotes;
+    };
+
+    async getMessage(id) {
+        const client = await this.getConnectedService();
+
+        const collection = client.db(process.env.DATABASE_NAME).collection('messages');
+        const quote = await collection.findOne({ _id: mongodb.ObjectID(id) });
+
+        await client.close();
+
+        return quote;
+    };
+}
+
+export default MessageService;
