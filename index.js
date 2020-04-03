beec2d7ff0ad1843877a61021aa9f0b61ce94967
Hugo CAPOCCI <h.capocci@gmail.com>
2020-04-03 10:39:57 +0200

    Add PUT route

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs').promises;
 const app = express();
 const v1 = express.Router();
 require('dotenv').config();
@@ -35,16 +34,12 @@ v1.get('/message/:id', async (request, response) => {
 
 v1.post('/message', basicAuth, async (request, response) => {
     const message = request.body;
-
-    // un message  est valide si il a un auteur et une citation
-    const isValid = message.quote && message.quote.length > 0
-     && message.author && message.author.length > 0;
     
-    if (!isValid) return response.sendStatus(400);
+    if (!MessageService.isMessageValid(message)) return response.sendStatus(400);
 
     // on sauvegarde dans mongo!
     const createdMessage = await messageService.createMessage(message);
-    response.send(createdMessage);
+    response.send(createdMessage); // 201
 });
 
 v1.delete('/message/:id', basicAuth, async (request, response) => {
@@ -53,6 +48,23 @@ v1.delete('/message/:id', basicAuth, async (request, response) => {
         const isDeleted = await messageService.deleteMessage(id);
         isDeleted ? response.sendStatus(204) : response.sendStatus(404);
     } catch(e) {
+        response.sendStatus(400);
+    }
+});
+
+v1.put('/message/:id', basicAuth, async (request, response) => {
+    const id = request.params.id;
+    const message = request.body;
+    if (!MessageService.isMessageValid(message))
+        return response.sendStatus(400);
+
+    try {
+        const result = await messageService.updateMessage(message, id);
+        if(!result.isFind) return response.sendStatus(404);
+        // 304 ou 202... 3xx correspond normalement à une redirection
+        result.isModified ? response.sendStatus(200) : response.sendStatus(304);
+    } catch(e) {
+        console.log('error occurs : ', e);
         response.sendStatus(400);
     }
 });
